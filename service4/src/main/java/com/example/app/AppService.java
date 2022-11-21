f8184040977a732e74024961c021e451ae81788b
BikashPandey17 <pandeybikash98@gmail.com>
2022-11-22 00:37:07 +0530

    improved API output

--- service4/src/main/java/com/example/app/AppService.java
+++ service4/src/main/java/com/example/app/AppService.java
@@ -7,11 +7,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
-import javax.validation.constraints.Null;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.*;
@@ -19,7 +15,7 @@ import static java.util.stream.Collectors.*;
 @Service
 public class AppService {
 
-    private static Logger logger = LoggerFactory.getLogger(AppService.class);
+    private static final Logger logger = LoggerFactory.getLogger(AppService.class);
     private final GuildTableRepository guildTableRepository;
 
     @Autowired
@@ -41,11 +37,16 @@ public class AppService {
 
     @LogMethodParam
     public Map getOne(int id) {
-        List<GuildTable> out = guildTableRepository.findByParentidOrId(id, id);
+//        List<GuildTable> out = guildTableRepository.findByParentidOrId(id, id);
+        long count = guildTableRepository.count();
+        Page<GuildTable> out = guildTableRepository.findAll(PageRequest.of(0, (int)count));
+        Map<Integer, List<GuildTable>> data = out.stream()
+                .filter(k -> k.getParentid() > 0)
+                .collect(Collectors.groupingBy(GuildTable::getParentid));
         try {
-            Map resp = Map.of(
-                    "Name", Objects.requireNonNull(out.stream().filter(k -> k.getId() == id).findFirst().orElse(null)),
-                    "Sub Class", out.stream().filter(k-> k.getId()!=id).collect(toList()));
+            Map<String, Object> resp = Map.of(
+                    "Name", out.stream().filter(k -> k.getId() == id).map(GuildTable::getName).findFirst().orElse(""),
+                    "Sub Class", AppService.getData(data, data.get(id)));
             return resp;
         } catch (NullPointerException nullPointerException) {
             logger.error("No record found for : " + id);
@@ -53,16 +54,33 @@ public class AppService {
         }
     }
 
-    public List getAll() {
+    public List<Map<String, Object>> getAll() {
         long count = guildTableRepository.count();
         Page<GuildTable> out = guildTableRepository.findAll(PageRequest.of(0, (int)count));
-        List resp = out.stream()
+        // worst case approach
+
+        Map<Integer, List<GuildTable>> data = out.stream()
                 .filter(k -> k.getParentid() > 0)
-                .collect(Collectors.groupingBy(GuildTable::getParentid))
-                .entrySet().stream()
-                .map(v1 -> Map.of("Name", Objects.requireNonNull(out.stream().filter(k -> k.getId() == v1.getKey()).map(GuildTable::getName).findFirst().orElse("")),
-                                  "Sub Classes", v1.getValue()))
-                .collect(toList());
-        return resp;
+                .collect(Collectors.groupingBy(GuildTable::getParentid));
+        ArrayList<Map<String, Object>> output = new ArrayList<>();
+
+        data.forEach((key, value) -> {
+            output.add(Map.of("Name", out.stream().filter(k -> k.getId() == key).map(GuildTable::getName).findFirst().orElse(""),
+                    "Sub Classes", AppService.getData(data, value)));
+                });
+        return output;
+    }
+
+    private static ArrayList<Map<String, Object>> getData(Map<Integer, List<GuildTable>> data, List<GuildTable> guildTables) {
+        ArrayList<Map<String, Object>> out = new ArrayList<>();
+        for(GuildTable guildTable: guildTables) {
+            if(data.containsKey(guildTable.getId())){
+                out.add(Map.of("Name", guildTable.getName(),
+                        "Sub Classes", getData(data, data.get(guildTable.getId()))));
+            } else {
+                out.add(Map.of("Name", guildTable.getName()));
+            }
+        }
+        return out;
     }
 }
